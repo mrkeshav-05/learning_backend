494edc99c577d93f6222a7aaf3bcbabc428e0b72
mrkeshav.05 <keshavpundhir69@gmail.com>
2024-05-29 17:34:35 +0530

    Regeneration of refresh token is completed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 
 // This will verify user is available or not
-
+// This is the verifyJWT method that will be used to verify the user's token 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
@@ -13,9 +13,10 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     }
     
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-  
+  // decodeToken has some things like - _id, payloads, etc.
+  // this is find the user by their id in the decodedToken
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-  
+  // checking the user exists or not exists
     if(!user){
       throw new ApiError(401, "Invalid Access Token")
     }
