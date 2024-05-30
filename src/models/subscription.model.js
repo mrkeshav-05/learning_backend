7684d129d4695213e6f4ca214e06aa6f9f5f4f9d
mrkeshav.05 <keshavpundhir69@gmail.com>
2024-05-30 16:56:00 +0530

    Add user model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+  subscriber:{
+    type: Schema.Types.ObjectId, //One who is subscribing
+    ref: "User"
+  },
+  channel:{
+    type: Schema.Types.ObjectId, //One whom to subscribe
+    ref: "User"
+  }
+}, { timestamps: true })
+
+export const Subscription = mongoose.model('Subscription', subscriptionSchema);
